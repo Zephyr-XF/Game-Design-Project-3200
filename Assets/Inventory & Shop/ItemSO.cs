8854385d784dd2eceb8beff0f8cf2aa9918efefc
Alex <2148668549@qq.com>
2025-11-26 23:02:28 +0800

    lootpickup

--- Assets/Inventory & Shop/ItemSO.cs
+++ Assets/Inventory & Shop/ItemSO.cs
@@ -9,6 +9,7 @@ public class ItemSO : ScriptableObject
     [TextArea] public string itemDescription;
     public Sprite icon;
     public bool isGold;
+    public int stackSize=3;
     [Header("Stats")]
     public int currentHealth;
     public int maxHealth;
--- Assets/Inventory & Shop/Loot.cs
+++ Assets/Inventory & Shop/Loot.cs
@@ -9,22 +9,42 @@ public class Loot : MonoBehaviour
     public ItemSO itemSO;
     public SpriteRenderer sr;
     public Animator anim;
+    public bool canBePickedUp = true;
     public int quantity;
     public static event Action<ItemSO, int> OnItemLooted;
     private void OnValidate()
+    // private void Awake()
     {
         if (itemSO == null)
             return;
+        UpdateAppearance();
+    }
+    public void Initialize(ItemSO itemSO, int quantity)
+    {
+        this.itemSO = itemSO;
+        this.quantity = quantity;
+        canBePickedUp = false;
+        UpdateAppearance();
+    }
+    private void UpdateAppearance()
+    {
         sr.sprite = itemSO.icon;
         this.name = itemSO.itemName;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && canBePickedUp == true)
         {
            anim.Play("LootPickup");
            OnItemLooted?.Invoke(itemSO, quantity);
            Destroy(gameObject, 0.5f);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canBePickedUp = true;
         }
     }
 }
