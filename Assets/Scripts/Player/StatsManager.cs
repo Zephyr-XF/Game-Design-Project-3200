5655dda011a1d40662409702e375d7179e31eafd
Alex <2148668549@qq.com>
2025-12-12 17:25:39 +0800

    选祝福会导致清醒值降低

--- Assets/Scripts/Player/StatsManager.cs
+++ Assets/Scripts/Player/StatsManager.cs
@@ -76,5 +76,7 @@ public class StatsManager : MonoBehaviour
         currentSanity += amount;
         // 使用 Mathf.Clamp 将值限制在 0 和 maxSanity 之间
         currentSanity = Mathf.Clamp(currentSanity, 0, maxSanity);
+        
+        if (statsUI != null) statsUI.UpdateAllStats();
     }
 }
